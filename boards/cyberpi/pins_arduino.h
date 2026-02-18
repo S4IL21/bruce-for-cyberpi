073f08452c3b04eb9e2bb885be47d1d6aa6e45d0
S4IL <126200245+S4IL21@users.noreply.github.com>
2026-02-18 22:38:23 +0100

    Update pins_arduino.h

--- boards/cyberpi/pins_arduino.h
+++ boards/cyberpi/pins_arduino.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// CyberPi 1.44" TFT SPI
+// SPI TFT Display 1.44" (128x128)
 #define PIN_MOSI 23
 #define PIN_MISO 19
 #define PIN_CLK 18
@@ -14,7 +14,14 @@
 #define PIN_BUTTON_B 34
 #define PIN_BUTTON_HOME 39
 
-// Optional peripherals
+// Builtin LED (optional)
+#define LED_BUILTIN 5
+
+// Peripherals (set -1 if unused)
 #define PIN_MIC -1
 #define PIN_CC1101 -1
 #define PIN_LORA -1
+
+// Arduino macros
+#define digitalWrite(pin, level) gpio_set_level((gpio_num_t)pin, level)
+#define digitalRead(pin) gpio_get_level((gpio_num_t)pin)
