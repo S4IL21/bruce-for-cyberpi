f298daf5e2c01746cee1c95b15bd4fa924f0e780
S4IL <126200245+S4IL21@users.noreply.github.com>
2026-02-18 22:35:48 +0100

    Update pins_arduino.h

--- boards/cyberpi/pins_arduino.h
+++ boards/cyberpi/pins_arduino.h
@@ -1,1 +1,20 @@
+#pragma once
 
+// CyberPi 1.44" TFT SPI
+#define PIN_MOSI 23
+#define PIN_MISO 19
+#define PIN_CLK 18
+#define PIN_CS 12
+#define PIN_DC 2
+#define PIN_RST 5
+#define PIN_BL 5
+
+// Buttons / Joystick
+#define PIN_BUTTON_A 35
+#define PIN_BUTTON_B 34
+#define PIN_BUTTON_HOME 39
+
+// Optional peripherals
+#define PIN_MIC -1
+#define PIN_CC1101 -1
+#define PIN_LORA -1
